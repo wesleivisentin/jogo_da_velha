ea0fd587e311507a3238eb4504dcee9342e09fbe
weslei <wesleivisentin@hotmail.com>
2022-07-02 13:58:47 -0300

    ticToe

--- interface.js
+++ interface.js
@@ -17,19 +17,34 @@ function handClick(event){
     let square = event.target
     let position = square.id
 
-    handleMove(position)
-    updateSquares()
+    if (handleMove(position)) {
+       
+
+        setTimeout(()=>{
+            alert("o jogo acabou!  O ganhador foi o jogador " + playerTime)
+        },10)
+        
+    }
+    updateSquare(position)
 }
+/////
 
-function updateSquares(){
-    let squares = document.querySelectorAll(".square")
 
-    squares.forEach((square) => {
-        let position = square.id
-        let symbol = board[position]
-
-        if (symbol != "") {
-            square.innerHTML = `<div class="${symbol}"></div>`
-        }
-    })
+function updateSquare(position) {
+    let square = document.getElementById(position.toString())
+    let symbol = board[position]
+    square.innerHTML = `<div class="${symbol}"></div>`
 }
+//mesma coisa que esse:
+// function updateSquares(){
+//     let squares = document.querySelectorAll(".square")
+
+//     squares.forEach((square) => {
+//         let position = square.id
+//         let symbol = board[position]
+
+//         if (symbol != "") {
+//             square.innerHTML = `<div class="${symbol}"></div>`
+//         }
+//     })
+// }
